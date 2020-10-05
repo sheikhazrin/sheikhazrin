dd894855736725896dc8904aa7ce26db8d93bac3
Sheik Hazrin Sheik Othman <sheikhazrin@outlook.my>
2020-10-06 04:04:34 +0800

    update readme using ts pt2

---
+++ config.ts
@@ -0,0 +1,155 @@
+export const CONFIG = {
+  social: [
+    {
+      name: 'Medium',
+      color: '12100E',
+      url: 'https://medium.com/@sheikhazrin',
+    },
+    {
+      name: 'Github',
+      username: 'sheikhazrin',
+      color: '12100E',
+      url: 'https://github.com/sheikhazrin',
+    },
+    {
+      name: 'Twitter',
+      color: '1DA1F2',
+      url: 'https://twitter.com/shazrinjb',
+    },
+    {
+      name: 'LinkedIn',
+      color: '0077B5',
+      url: 'https://www.linkedin.com/in/sheikhazrin',
+    },
+    {
+      name: 'Website',
+      color: '4285F4',
+      logo: 'google-chrome',
+      url: 'https://dev.to/sheikhazrin',
+    },
+    // {
+    //   name: 'Instagram',
+    //   color: 'E4405F',
+    //   url: 'https://www.instagram.com/username',
+    // },
+    // {
+    //   name: 'Facebook',
+    //   color: '1877F2',
+    //   url: 'https://www.facebook.com/username',
+    // },
+  ],
+  github: {
+    username: 'sheikhazrin',
+    colors: {
+      title: '24292e',
+      text: '24292e',
+      icon: '24292e',
+      background: 'ffffff',
+    },
+    stats: {
+      mostUsedLanguages: true,
+      overallStats: true,
+    },
+    highlightedRepos: ['sheikhazrin'],
+  },
+  instagram: {
+    enabled: false,
+    username: 'sheikhazrin',
+    numberOfImages: 3,
+  },
+  mediumArticles: {
+    enabled: true,
+    username: 'sheikhazrin',
+    numberOfArticles: 3,
+  },
+  badges: {
+    enabled: true,
+    spectrum: ['46a2f1', '311C87', 'DD0031', '43853d'],
+    list: [
+  
+      {
+        name: 'Vue',
+        logo: 'vue.js',
+      },
+      {
+        name: 'Laravel',
+        logo: 'laravel',
+      },
+      {
+        name: 'React',
+      },
+      {
+        name: 'NextJS', 
+        logo: 'next.js',
+
+      }, 
+      {
+        name: 'NuxtJS', 
+        logo: 'nuxt.js',
+
+      },
+
+      {
+        name: 'Webpack',
+      },
+      {
+        name: 'Docker',
+      },
+      {
+        name: 'Typescript',
+      },
+      {
+        name: 'Javascript',
+      },
+      {
+        name: 'Redux',
+      },
+      {
+        name: 'Sass',
+      },
+      {
+        name: 'Git',
+      },
+      {
+        name: 'AWS',
+        logo: 'amazon-aws',
+      },
+      {
+        name: 'Python',
+      },
+      {
+        name: 'Ruby on Rails',
+        logo: 'ruby-on-rails',
+      },
+      {
+        name: 'Github',
+      },
+      {
+        name: 'npm',
+      },
+      {
+        name: 'HTML5',
+      },
+      {
+        name: 'Bootstrap',
+      },
+      
+      {
+        name: 'MongoDB',
+      },
+      {
+        name: 'PostgreSQL',
+      },
+      {
+        name: 'MySQL',
+      },
+      {
+        name: 'Prettier',
+      },
+      {
+        name: 'NodeJS',
+        logo: 'Node.js',
+      },
+    ],
+  },
+};
